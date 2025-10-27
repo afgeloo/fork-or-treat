f7a7ceda8be53e4886973e7ac3c89801bc8a6472
Angelo Reyes <afgeloo@gmail.com>
2025-10-28 01:43:03 +0800

    chore: last try reflecting changes to vercel preview

--- profile.js
+++ profile.js
@@ -1,7 +1,7 @@
 // profile.js
 window.PROFILE = {
   avatar: "assets/img/profile.jpeg",
-  name: "France geloo Joel M. Reyes",
+  name: "France Angelo Joel M. Reyes",
   role: "Aspiring Software Engineer",
   bio: "carpe diem",
   tags: ["Git", "GitHub", "Python", "Nextjs", "Reactjs", "React Native", "Flutter", "Data Science", "Machine Learning"],
