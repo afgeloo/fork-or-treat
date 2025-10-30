b06dc14f79b3a86209f54f8bb3aa0c85a01ae513
rheician <gonzalesrheicianjhoy@gmail.com>
2025-10-30 16:31:29 +0800

    New commit message

--- profile.js
+++ profile.js
@@ -10,7 +10,7 @@ window.PROFILE = {
     { label: "Favorite Food", value: "Ice Cream" },
     { label: "Course",        value: "BS Information Technology" },
     { label: "College",       value: "CISTM" },
-    { label: "Hobby",         value: "sleep" },
+    { label: "Hobby",         value: "Sleep" },
     { label: "Location",      value: "Manila" }
   ]
 };
