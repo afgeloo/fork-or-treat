12037d168ed82b1a1026cab092a5dbf14a039e2f
Jody Erich Medel <jodymedel@gmail.com>
2025-10-30 16:17:12 +0800

    Changed by Jody Medel

--- profile.js
+++ profile.js
@@ -1,16 +1,16 @@
 // profile.js
 window.PROFILE = {
-  avatar: "assets/img/profile.png",
-  name: "Wizard Honeydroid",
-  role: "Aspiring ML Engineer",
+  avatar: "assets/img/WITHOUT NAME PICTURE.jpg",
+  name: "Jody Erich S. Medel",
+  role: "A 1st Year Computer Science Student, Aspiring Developer, and Tech Enthusiast, a nerd, foodie, and a gamer.",
   bio: "Tech birdie who loves coding, tidy repos, and applying what I learn.",
-  tags: ["Git", "GitHub", "HTML", "CSS", "JavaScript", "Web Development", "Data Science", "Machine Learning"],
+  tags: ["Git", "GitHub", "HTML", "CSS", "C", "Computer Science", "Nerd", "Foodie", "Gamer"],
   facts: [
-    { label: "Nickname",      value: "Bentong" },
-    { label: "Favorite Food", value: "Tonkatsu 🍱" },
+    { label: "Nickname",      value: "Jody" },
+    { label: "Favorite Food", value: "Dumplings" },
     { label: "Course",        value: "BS Computer Science" },
     { label: "College",       value: "CISTM" },
-    { label: "Hobby",         value: "Pixel art 🎨" },
+    { label: "Hobby",         value: "Gamer 🎮" },
     { label: "Location",      value: "Manila, PH 📍" }
   ]
 };
