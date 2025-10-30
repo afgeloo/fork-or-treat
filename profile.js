9d1787c484aa0318f21ef1bb747cd54c6dfbe44a
Axel Rose Alcober <artalcoberr@gmail.com>
2025-10-30 15:24:02 +0800

    Updated profile and added avatar

--- profile.js
+++ profile.js
@@ -1,16 +1,16 @@
 // profile.js
 window.PROFILE = {
-  avatar: "assets/img/profile.png",
-  name: "Wizard Honeydroid",
-  role: "Aspiring ML Engineer",
-  bio: "Tech birdie who loves coding, tidy repos, and applying what I learn.",
+  avatar: "assets/img/pfp-1.jpeg",
+  name: "Wizard Axel",
+  role: "Aspiring CS Freshie",
+  bio: "Kick drum, chew gum.",
   tags: ["Git", "GitHub", "HTML", "CSS", "JavaScript", "Web Development", "Data Science", "Machine Learning"],
   facts: [
-    { label: "Nickname",      value: "Bentong" },
-    { label: "Favorite Food", value: "Tonkatsu 🍱" },
+    { label: "Nickname",      value: "Niño" },
+    { label: "Favorite Food", value: "Fudgee Bar" },
     { label: "Course",        value: "BS Computer Science" },
     { label: "College",       value: "CISTM" },
-    { label: "Hobby",         value: "Pixel art 🎨" },
+    { label: "Hobby",         value: "Movie watching 🎨" },
     { label: "Location",      value: "Manila, PH 📍" }
   ]
 };
