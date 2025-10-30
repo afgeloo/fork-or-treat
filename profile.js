b03ba4bdc475dac845017fd016f6817ab6064377
Carlos Miguel Paterno <cmp789@gmail.com>
2025-10-30 15:25:47 +0800

    changed values in profile.js

--- profile.js
+++ profile.js
@@ -1,16 +1,16 @@
 // profile.js
 window.PROFILE = {
-  avatar: "assets/img/profile.png",
-  name: "Wizard Honeydroid",
-  role: "Aspiring ML Engineer",
-  bio: "Tech birdie who loves coding, tidy repos, and applying what I learn.",
-  tags: ["Git", "GitHub", "HTML", "CSS", "JavaScript", "Web Development", "Data Science", "Machine Learning"],
+  avatar: "creating-own-repository-/assets/img/416536582_385856903997629_7477550748828878131_n.jpg",
+  name: "Soobin",
+  role: "Rolling in the Deep",
+  bio: "Freshie baddie ng CS",
+  tags: ["C", "HTML", "CSS"],
   facts: [
-    { label: "Nickname",      value: "Bentong" },
-    { label: "Favorite Food", value: "Tonkatsu 🍱" },
+    { label: "Nickname",      value: "soob" },
+    { label: "Favorite Food", value: "Fries" },
     { label: "Course",        value: "BS Computer Science" },
     { label: "College",       value: "CISTM" },
-    { label: "Hobby",         value: "Pixel art 🎨" },
-    { label: "Location",      value: "Manila, PH 📍" }
+    { label: "Hobby",         value: "Watching TiKToks" },
+    { label: "Location",      value: "Manila, PH 📍" },
   ]
 };
