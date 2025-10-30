61e6cd7cd500512b088139d24e53495272413cc4
xivinas <xivinas5@gmail.com>
2025-10-30 16:23:16 +0800

    changed values in avatar, role, fav food, and hobby.

--- profile.js
+++ profile.js
@@ -1,16 +1,16 @@
 // profile.js
 window.PROFILE = {
-  avatar: "assets/img/profile.png",
+  avatar: "assets/img/profile1.png",
   name: "Jonas Calderon",
-  role: "Aspiring ML Engineer",
-  bio: "Tech birdie who loves coding, tidy repos, and applying what I learn.",
-  tags: ["Git", "GitHub", "HTML", "CSS", "JavaScript", "Web Development", "Data Science", "Machine Learning"],
+  role: "Aspiring Mobile Developer",
+  bio: "Tech newbie that finds answers in life through coding.",
+  tags: ["Git", "GitHub", "HTML", "CSS", "JavaScript", "Mobile Development", "Web Development"],
   facts: [
-    { label: "Nickname",      value: "Bentong" },
-    { label: "Favorite Food", value: "Tonkatsu 🍱" },
+    { label: "Nickname",      value: "Jones" },
+    { label: "Favorite Food", value: "Katsudon 🍱" },
     { label: "Course",        value: "BS Computer Science" },
     { label: "College",       value: "CISTM" },
-    { label: "Hobby",         value: "Pixel art 🎨" },
+    { label: "Hobby",         value: "Playing acoustic guitar" },
     { label: "Location",      value: "Manila, PH 📍" }
   ]
 };
