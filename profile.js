83c004ce844cfa4e79aaf48c097fd34e1cbc0051
Vhalerie Lei <vhalerielei21@gmail.com>
2025-10-30 15:34:33 +0800

    Reverted info

--- profile.js
+++ profile.js
@@ -1,16 +1,16 @@
 // profile.js
 window.PROFILE = {
   avatar: "assets/img/profile.png",
-  name: "Vhalerie Lei Comeros",
-  role: "Aspiring UI/UX Designer & Anything Web Development",
-  bio: "A tech girlie who loves to learn code, explore anything new, and branch out.",
+  name: "Wizard Honeydroid",
+  role: "Aspiring ML Engineer",
+  bio: "loves to learn code, explore anything new, and branch out.",
   tags: ["Git", "GitHub", "HTML", "CSS", "JavaScript", "Web Development", "UI/UX", "Web Development"],
   facts: [
-    { label: "Nickname",      value: "Vhal" },
-    { label: "Favorite Food", value: "Sushi & Strawberries!" },
+    { label: "Nickname",      value: "Bentong" },
+    { label: "Favorite Food", value: "Tonkatsu" },
     { label: "Course",        value: "BS Computer Science" },
     { label: "College",       value: "CISTM" },
-    { label: "Hobby",         value: "Reading & Anything Art" },
+    { label: "Hobby",         value: "Pixel Art" },
     { label: "Location",      value: "Manila, PH 📍" }
   ]
 };
