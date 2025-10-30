899963761b3fab4f9330b5ed72f37567ef9d5dbb
N-Jacques <njroxas613@gmail.com>
2025-10-30 15:39:29 +0800

    change values of profile.js  and add pic to assets

--- profile.js
+++ profile.js
@@ -1,13 +1,13 @@
 // profile.js
 window.PROFILE = {
-  avatar: "assets/img/profile.png",
-  name: "Wizard Honeydroid",
-  role: "Aspiring ML Engineer",
-  bio: "Tech birdie who loves coding, tidy repos, and applying what I learn.",
+  avatar: "assets/img/Nicco_pic.jpg",
+  name: "Niccolo Jacques Roxas",
+  role: "Aspiring QA Analyst",
+  bio: "Break stuff, unsuccessful? Good",
   tags: ["Git", "GitHub", "HTML", "CSS", "JavaScript", "Web Development", "Data Science", "Machine Learning"],
   facts: [
-    { label: "Nickname",      value: "Bentong" },
-    { label: "Favorite Food", value: "Tonkatsu 🍱" },
+    { label: "Nickname",      value: "Nicco" },
+    { label: "Favorite Food", value: "Sisig" },
     { label: "Course",        value: "BS Computer Science" },
     { label: "College",       value: "CISTM" },
     { label: "Hobby",         value: "Pixel art 🎨" },
