6274aa5de7abf9be3d84e26c74004a9228400154
Daniel Camacho <danielcamacho0416@gmail.com>
2025-10-30 13:02:21 +0800

    Daniel inda houwwwzzz

--- profile.js
+++ profile.js
@@ -1,16 +1,16 @@
 // profile.js
 window.PROFILE = {
-  avatar: "assets/img/profile.png",
-  name: "Wizard Honeydroid",
-  role: "Aspiring ML Engineer",
-  bio: "Tech birdie who loves coding, tidy repos, and applying what I learn.",
-  tags: ["Git", "GitHub", "HTML", "CSS", "JavaScript", "Web Development", "Data Science", "Machine Learning"],
+  avatar: "assets/img/me.png",
+  name: "Daniel Camacho",
+  role: "Web Development Lead",
+  bio: "Just a chill guy, really.",
+  tags: ["Git", "Python", "TypeScript", "JavaScript", "React", "Web Development", "Software Engineering", "Machine Learning"],
   facts: [
-    { label: "Nickname",      value: "Bentong" },
-    { label: "Favorite Food", value: "Tonkatsu 🍱" },
+    { label: "Nickname",      value: "Hajin" },
+    { label: "Favorite Food", value: "Kimchi" },
     { label: "Course",        value: "BS Computer Science" },
     { label: "College",       value: "CISTM" },
-    { label: "Hobby",         value: "Pixel art 🎨" },
-    { label: "Location",      value: "Manila, PH 📍" }
+    { label: "Hobby",         value: "Sleeping" },
+    { label: "Location",      value: "Manila, PH" }
   ]
 };
