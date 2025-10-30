6807f695b5543e10c466480411a0e5750f7f7754
Axel Rose Alcober <artalcoberr@gmail.com>
2025-10-30 15:51:20 +0800

    Updated my info in profile.js

--- profile.js
+++ profile.js
@@ -3,14 +3,14 @@ window.PROFILE = {
   avatar: "assets/img/pfp-1.jpeg",
   name: "Wizard Axel",
   role: "Aspiring CS Freshie",
-  bio: "Kick drum, chew gum.",
-  tags: ["Git", "GitHub", "HTML", "CSS", "JavaScript", "Web Development", "Data Science", "Machine Learning"],
+  bio: "Surrender to the moment.",
+  tags: ["Git", "GitHub", "HTML", "CSS", "C", "Web Development"],
   facts: [
     { label: "Nickname",      value: "Niño" },
     { label: "Favorite Food", value: "Fudgee Bar" },
     { label: "Course",        value: "BS Computer Science" },
     { label: "College",       value: "CISTM" },
-    { label: "Hobby",         value: "Movie watching 🎨" },
+    { label: "Hobby",         value: "Movie watching  🍿" },
     { label: "Location",      value: "Manila, PH 📍" }
   ]
 };
