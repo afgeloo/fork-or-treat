69f52b47e777550ef68954b977bd593cc769aaae
rm-cg <guinto36ruth@gmail.com>
2025-10-30 12:20:08 +0800

    Add Ruth's profile

--- profile.js
+++ profile.js
@@ -1,16 +1,16 @@
 // profile.js
 window.PROFILE = {
-  avatar: "assets/img/profile.png",
+  avatar: "assets/img/Ruth.jpg",
   name: "Wizard Honeydroid",
   role: "Aspiring ML Engineer",
   bio: "Tech birdie who loves coding, tidy repos, and applying what I learn.",
   tags: ["Git", "GitHub", "HTML", "CSS", "JavaScript", "Web Development", "Data Science", "Machine Learning"],
   facts: [
-    { label: "Nickname",      value: "Bentong" },
-    { label: "Favorite Food", value: "Tonkatsu 🍱" },
-    { label: "Course",        value: "BS Computer Science" },
-    { label: "College",       value: "CISTM" },
-    { label: "Hobby",         value: "Pixel art 🎨" },
+    { label: "Nickname",      value: "Marga" },
+    { label: "Favorite Food", value: "Sushi 🍣" },
+    { label: "Course",        value: "BS Mathematics" },
+    { label: "College",       value: "CS" },
+    { label: "Hobby",         value: "Journaling 📝" },
     { label: "Location",      value: "Manila, PH 📍" }
   ]
 };
